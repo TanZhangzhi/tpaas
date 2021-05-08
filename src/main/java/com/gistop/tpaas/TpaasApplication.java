d7372249b511878a64bf8a9064338e3535e86421
tanzz <tanzhangz@sutpc.com>
2021-05-08 22:01:12 +0800

    First commit on tPaaS backend

---
+++ src/main/java/com/gistop/tpaas/TpaasApplication.java
@@ -0,0 +1,13 @@
+package com.gistop.tpaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gistop/tpaas/TpaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gistop.tpaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
